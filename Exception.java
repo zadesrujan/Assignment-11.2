3019e418fd83b675c194e5d050cf8829a30c8cf4
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-10-13 15:59:40 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,31 @@
+package divide;
+//here we cretaed package as divide which that organizes a set of related classes and interfaces.
+public class Exception {
+
+	public static void main(String[] args) {
+		//public : it can be called from anywhere.
+		//static : doesn't belong to a specific object.
+		//void : returns to no value.
+		//main : is special because it will start the program.
+		// TODO Auto-generated method stub
+
+		int s,v,z;
+		//giving 3 integer variables and their values
+		s=84;
+		v=0;
+		z=s/v;
+		System.out.println(z);
+				//system : is a class in java language pacakge.
+				//out : static member of the system class.
+				//println : to print what is output.
+	/*we get Arithmentic exception as an error
+	 *if the divisor(denominator) is 0,it is undefined,here Jvm is
+	 *unable to evaluate the result and thereby terminates the execution.
+	 *	
+	 */
+	}
+
+
+	}
+
+
